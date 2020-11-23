58d769fafd6b20a99de6e60f69bad41b02e01979
aditimarathe14 <aditimarathe1@gmail.com>
2020-11-23 18:58:34 +0530

    eject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from './counter'
 function App() {
   return (
-    <div className="App">
-     <h1>Learn react testing  </h1>
+    <div  data-test="component-app" className="App">
+     
+     <h1>Learn react testing  !! </h1>
+     <Counter></Counter>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,31 @@
-//import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
+import Enzyme ,{shallow} from 'enzyme'
+import EnzymeAdapter  from 'enzyme-adapter-react-16'
+Enzyme.configure({adapter: new EnzymeAdapter()})  
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+ const wrapper = shallow(<App/>)
+//expect(wrapper).toBeTruthy();
+const appComponent = wrapper.find("[data-test='component-app']")
+expect(appComponent.length).toBe(1)
+});
 
 
 test('render without error ',()=>{
-  
+  const wrapper = shallow(<App/>)
+  expect(wrapper).toBeTruthy();
+})
+
+test('render button ',()=>{
+
+})
+
+test('render counter display ',()=>{
+
+})
+
+test('clicking on button incriment counter display ',()=>{
+
 })
